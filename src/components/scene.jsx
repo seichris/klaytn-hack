a6d43a65aaf43dda1d688928e1c9d871bc2d12fe
seichris <cerujurasa@gmail.com>
2023-11-24 09:58:27 +0200

    update sky design

--- src/components/scene.jsx
+++ src/components/scene.jsx
@@ -1,16 +1,45 @@
 import React, { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
+import { Sky, Cloud } from "@react-three/drei";
 import { Model } from "./threejs/Isometric-cityscape13";
 import Loader from "./Loader";
+import { Environment } from "@react-three/drei";
 
 function Scene() {
   const [buttonText, setButtonText] = useState(null);
+  const skySettings = {
+    distance: 450000,
+    inclination: 0, // Adjust for time of day, affects sky color
+    azimuth: 0.25, // Adjust for sun position
+    turbidity: 0.5, // Affects the intensity of the sky color
+    rayleigh: 3, // Determines the amount and color of scattered light
+    mieCoefficient: 0.005, // Affects the concentration of haze particles
+    mieDirectionalG: 0.8 // Affects the directionality of haze scattering
+  };
+
   return (
-    <div className=' relative w-screen h-screen'>
+    <div className='relative w-screen h-screen'>
       <Canvas>
+        <Sky
+          distance={skySettings.distance}
+          sunPosition={[0, 1, 0]}
+          inclination={skySettings.inclination}
+          azimuth={skySettings.azimuth}
+          turbidity={skySettings.turbidity}
+          rayleigh={skySettings.rayleigh}
+          mieCoefficient={skySettings.mieCoefficient}
+          mieDirectionalG={skySettings.mieDirectionalG}
+        />
+        {/* <Cloud
+          position={[0, 2, 0]} // Adjust cloud position as needed
+          speed={0.4} // Speed of cloud movement
+          opacity={0.6} // Cloud opacity
+          width={10} // Width of the cloud layer
+          depth={1.5} // Depth of the cloud layer
+          segments={20} // Number of segments in the cloud mesh
+        /> */}
         <fog attach='fog' args={["#fff", 0, 110]} />
-        <Environment preset='forest' />
+        {/* <Environment preset='forest' />  */}
         <ambientLight color={0xe8c37b} intensity={2} />
         <directionalLight
           position={[-10, 10, 10]}
@@ -27,7 +56,7 @@ function Scene() {
         </Suspense>
       </Canvas>
       {buttonText && (
-        <div className='bg-white px-7 rounded-3xl flex py-2 justify-center text-black absolute bottom-10 left-[50%] translate-x-[-50%] '>
+        <div className='bg-white px-7 rounded-3xl flex py-2 justify-center text-black absolute bottom-10 left-[50%] translate-x-[-50%]'>
           <span className='text-xl uppercase font-semibold'>{buttonText}</span>
         </div>
       )}
